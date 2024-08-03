dc02ec63864d152fa643bcc9a07dd0a78de818fe
Ayushi Tyagi <tyagi.ayushi960@gmail.com>
2024-08-03 17:43:39 +0530

    Resolved merge conflicts

--- src/main/java/com/learnkafka/LibraryEventsProducerApplication.java
+++ src/main/java/com/learnkafka/LibraryEventsProducerApplication.java
@@ -8,7 +8,6 @@ public class   LibraryEventsProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryEventsProducerApplication.class, args);
-		// this is the main commeent tushar 
-		// Changes added by Ayushi Tyagi   tushar's version
+		// Changes added by Ayushi Tyagi
 	}
 }
